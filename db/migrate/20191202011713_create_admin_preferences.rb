2476c5d13468764327717d4fd5bf476cc18d5ed9
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-02 01:21:21 +0000

    Automatically backed up by Learn

--- db/migrate/20191202011713_create_admin_preferences.rb
+++ db/migrate/20191202011713_create_admin_preferences.rb
@@ -1,7 +1,8 @@
 class CreateAdminPreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :admin_preferences do |t|
-
+      t.string :add_new_songs
+      t.string :add_new_artists
       t.timestamps
     end
   end
